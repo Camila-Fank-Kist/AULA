08102446125baa684c2b98a09510f0151e6f8af2
Camila-Fank-Kist <camila.fank19@gmail.com>
2025-02-14 16:09:13 -0300

    single-item layout

--- Aula 1/Meu Gabarito da Aula 1/src/components/Main.jsx
+++ Aula 1/Meu Gabarito da Aula 1/src/components/Main.jsx
@@ -11,14 +11,135 @@ const Main = () => {
           <a className='item-list__link' href="/">Mostrar tudo</a>
         </div>
 
-        <div className='single-item'> {/* bloco/componente */}
-          <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista X" />
-          {/* <FontAwesomeIcon icon="fa-solid fa-circle-play" /> */}
-          <FontAwesomeIcon icon={faCirclePlay} /> {/* assim traz outro componente: a gente importa tanto o componente FontAwesomeIcon quando o ícone, como se ele fosse uma imagem */}
-          <p>Henrique & Juliano</p>
-          <p>Artista</p>
-        </div>
+        <div className='item-list__container'>
 
+          <div className='single-item'> {/* bloco/componente */}
+            <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista X" />
+              </div>
+              {/* <FontAwesomeIcon icon="fa-solid fa-circle-play" /> */}
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} /> {/* assim traz outro componente: a gente importa tanto o componente FontAwesomeIcon quando o ícone, como se ele fosse uma imagem */}
+            </div>
+
+            <div className='single-item__texts'>
+              <div className='single-item__2lines'>
+                <p className='single-item__title'>Henrique & Juliano teste teste teste teste teste teste</p>
+              </div>
+              <p className='single-item__type'>Artista</p>
+            </div>
+              
+          </div>
+
+          <div className='single-item'> {/* bloco/componente */}
+            <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista X" />
+              </div>
+              {/* <FontAwesomeIcon icon="fa-solid fa-circle-play" /> */}
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} /> {/* assim traz outro componente: a gente importa tanto o componente FontAwesomeIcon quando o ícone, como se ele fosse uma imagem */}
+            </div>
+
+            <div className='single-item__texts'>
+              <div className='single-item__2lines'>
+                <p className='single-item__title'>Henrique & Juliano teste teste teste teste teste teste</p>
+              </div>
+              <p className='single-item__type'>Artista</p>
+            </div>
+              
+          </div>
+
+          <div className='single-item'> {/* bloco/componente */}
+            <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista X" />
+              </div>
+              {/* <FontAwesomeIcon icon="fa-solid fa-circle-play" /> */}
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} /> {/* assim traz outro componente: a gente importa tanto o componente FontAwesomeIcon quando o ícone, como se ele fosse uma imagem */}
+            </div>
+
+            <div className='single-item__texts'>
+              <div className='single-item__2lines'>
+                <p className='single-item__title'>Henrique & Juliano teste teste teste teste teste teste</p>
+              </div>
+              <p className='single-item__type'>Artista</p>
+            </div>
+              
+          </div>
+
+          <div className='single-item'> {/* bloco/componente */}
+            <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista X" />
+              </div>
+              {/* <FontAwesomeIcon icon="fa-solid fa-circle-play" /> */}
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} /> {/* assim traz outro componente: a gente importa tanto o componente FontAwesomeIcon quando o ícone, como se ele fosse uma imagem */}
+            </div>
+
+            <div className='single-item__texts'>
+              <div className='single-item__2lines'>
+                <p className='single-item__title'>Henrique & Juliano teste teste teste teste teste teste</p>
+              </div>
+              <p className='single-item__type'>Artista</p>
+            </div>
+              
+          </div>
+
+          <div className='single-item'> {/* bloco/componente */}
+            <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista X" />
+              </div>
+              {/* <FontAwesomeIcon icon="fa-solid fa-circle-play" /> */}
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} /> {/* assim traz outro componente: a gente importa tanto o componente FontAwesomeIcon quando o ícone, como se ele fosse uma imagem */}
+            </div>
+
+            <div className='single-item__texts'>
+              <div className='single-item__2lines'>
+                <p className='single-item__title'>Henrique & Juliano teste teste teste teste teste teste</p>
+              </div>
+              <p className='single-item__type'>Artista</p>
+            </div>
+              
+          </div>
+
+          <div className='single-item'> {/* bloco/componente */}
+            <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista X" />
+              </div>
+              {/* <FontAwesomeIcon icon="fa-solid fa-circle-play" /> */}
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} /> {/* assim traz outro componente: a gente importa tanto o componente FontAwesomeIcon quando o ícone, como se ele fosse uma imagem */}
+            </div>
+
+            <div className='single-item__texts'>
+              <div className='single-item__2lines'>
+                <p className='single-item__title'>Henrique & Juliano teste teste teste teste teste teste</p>
+              </div>
+              <p className='single-item__type'>Artista</p>
+            </div>
+              
+          </div>
+
+          <div className='single-item'> {/* bloco/componente */}
+            <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista X" />
+              </div>
+              {/* <FontAwesomeIcon icon="fa-solid fa-circle-play" /> */}
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} /> {/* assim traz outro componente: a gente importa tanto o componente FontAwesomeIcon quando o ícone, como se ele fosse uma imagem */}
+            </div>
+
+            <div className='single-item__texts'>
+              <div className='single-item__2lines'>
+                <p className='single-item__title'>Henrique & Juliano teste teste teste teste teste teste</p>
+              </div>
+              <p className='single-item__type'>Artista</p>
+            </div>
+              
+          </div>
+
+        </div>
       </div>    
     </div>
   )
