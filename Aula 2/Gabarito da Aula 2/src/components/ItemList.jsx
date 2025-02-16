e1fe11869063bb78eb4510727d8fff6f474be2bd
Camila-Fank-Kist <camila.fank19@gmail.com>
2025-02-15 23:19:39 -0300

    dynamic serving Main

--- Aula 2/Gabarito da Aula 2/src/components/ItemList.jsx
+++ Aula 2/Gabarito da Aula 2/src/components/ItemList.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import SingleItem from './SingleItem'
 
-const ItemList = (props) => {
-
-  console.log(props);
+// a função recebe como parâmetro o objeto "props"
+const ItemList = ( { title, items } ) => { // desestruturação: a variável "title" vai armazenar o valor da variável de mesmo nome do objeto "props"
+ 
+  // console.log("title: ", title);
+  // console.log("items: ", items);
 
   return (
     <div className='item-list'> {/* lista de itens */} {/* bloco/componente */}
       <div className='item-list__header'> {/* elemento do bloco item-list */}
-        <h2>Artistas populares</h2>
+        <h2>{title} populares</h2> {/* sempre que eu quero escrever js no meio do jsx, eu uso chaves */}
         <a className='item-list__link' href="/">Mostrar tudo</a>
       </div>
 
       <div className='item-list__container'>
 
-        <SingleItem />
-        <SingleItem />
-        <SingleItem />
-        <SingleItem />
-        <SingleItem />
+        {Array(items).fill().map( () => <SingleItem /> )}
+        {/* tá criando um array com 'items' elementos */}
+        {/* tá preenchendo todos os elementos do array com 'undefined' */}
+        {/* map: percorre cada elemento do array e retorna um array de mesmo tamanho */}
+        {/* Dentro do map tem uma arrow function. Os parâmeteos da função estão entre parênteses, e a função está retornando <SingleItem /> */}
+        {/* o map tá preenchendo cada índice do array com o retorno da arrow function -> o resultado do map é um novo array que contém 'items' instâncias do componente '<SingleItem />' */}
+        {/* se o map só retorna um array, como <SingleItem /> é exibido na tela? */}
+        {/* Renderização no React: Quando você coloca essa expressão JSX dentro de um componente React, o React renderiza o array resultante. O React é capaz de lidar com arrays de elementos JSX e renderizá-los como uma lista de componentes na interface do usuário. Portanto, mesmo que o map retorne um array, o React renderiza cada um dos elementos desse array na tela. */}
 
       </div>
     </div> 
--- Aula 2/Gabarito da Aula 2/src/components/Main.jsx
+++ Aula 2/Gabarito da Aula 2/src/components/Main.jsx
@@ -8,10 +8,10 @@ const Main = () => {
 
       {/* esses ItemList são o mesmo componente, mas com algumas modificações - para eles serem diferentes, tenho que passar uma informação para um e uma informação para outro */}
       {/* ItemList de Artistas */}
-      <ItemList title="Artistas Populares" /> 
+      <ItemList title="Artistas" items={4} /> {/* escrevendo js para passar o número 5 e não o texto 5 */}
       
       {/* ItemList de Músicas */}
-      <ItemList title="Músicas Populares" />   
+      <ItemList title="Músicas" items={8} />   
 
     </div>
   )
